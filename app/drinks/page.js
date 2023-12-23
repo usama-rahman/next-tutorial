a2909d9c5e7c4372d72aac63f22419bb8822b553
Usama Rahman <95021960+usama-rahman@users.noreply.github.com>
2023-12-23 19:40:57 +0600

    @

--- app/drinks/page.js
+++ app/drinks/page.js
@@ -1,4 +1,9 @@
-const DrinksPage = () => {
+const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a";
+
+const DrinksPage = async () => {
+  const res = await fetch(url);
+  const data = await res.json();
+  console.log(data);
   return (
     <div>
       <h1 className="text-xl">DrinksPage</h1>
