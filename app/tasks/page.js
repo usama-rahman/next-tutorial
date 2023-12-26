466350ce0cd309727c56d0abd04cf572898e780b
Usama Rahman <95021960+usama-rahman@users.noreply.github.com>
2023-12-26 19:13:18 +0600

    ee

--- app/tasks/page.js
+++ app/tasks/page.js
@@ -1,7 +1,30 @@
+"use client";
+
+import { useState } from "react";
+
 const TaskPage = () => {
+  const [title, setTitle] = useState("");
+
+  const handelSubmit = (e) => {
+    e.preventdefault();
+  };
+
   return (
     <div>
-      <h1 className="text-xl">TaskPage</h1>
+      <form className="flex justify-between w-4/5" onSubmit={handelSubmit}>
+        <input
+          type="text"
+          className="border-2 rounded-xl w-96 h-10 outline-none px-3 text-xl"
+          onChange={(e) => {
+            setTitle(e.target.value);
+            console.log(title);
+          }}
+        />
+        <button className="bg-blue-700 px-4  text-gray-200 rounded-md ">
+          Add
+        </button>
+      </form>
+      <hr className="mt-8" />
     </div>
   );
 };
